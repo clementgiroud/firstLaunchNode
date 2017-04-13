42fa802e3f0082b4d5e01b6418e90e365b764cb0
Your Name <giroudclement.devweb@gmail.com>
2017-04-13 12:30:42 +0200

    marche

--- server.js
+++ server.js
@@ -1,18 +1,35 @@
-var http = require('http');
+// var http = require('http');
+//
+// var ip = '127.0.0.1';
+// var port = 12107;
+//
+// var server = http.createServer(
+//     function(request,response){
+//         response.statusCode = 200;
+//         response.setHeader('Content-Type','text/plain');
+//         response.end('bkjbdvksjkv');
+//     }
+// );
+//
+// server.listen(port,ip,function(){
+//     console.log('le serveur tourne');
+//
+//     }
+// );
 
-var ip = '127.0.0.1';
-var port = 12107;
+var express = require('express');
 
-var server = http.createServer(
-    function(request,response){
-        response.statusCode = 200;
-        response.setHeader('Content-Type','text/plain');
-        response.end('bkjbdvksjkv');
-    }
-);
+var serveur = express();
+var routeToto = "/toto";
 
-server.listen(port,ip,function(){
-    console.log('le serveur tourne');
+serveur.get("/",function(request,response){
+    response.sendFile(__dirname + "/views/index.html");
+});
+serveur.get(routeToto,function(request,response){
+    response.send("message venant de toto");
+});
+serveur.post("/toto",function(request,response){
+    response.send("message venant de toto en post");
+});
 
-    }
-);
+serveur.listen(12107);
